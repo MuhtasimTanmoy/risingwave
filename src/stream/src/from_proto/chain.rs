ec3728e5f9338bcf0569c4784e35f3054e9b0bff
Noel Kwan <noelkwan1998@gmail.com>
2023-06-19 17:23:24 +0800

    add upstream_state_table field

--- src/stream/src/from_proto/chain.rs
+++ src/stream/src/from_proto/chain.rs
@@ -170,6 +170,7 @@ impl ExecutorBuilder for ChainExecutorBuilder {
                 .boxed()
             }
             ChainType::ChainUnspecified => unreachable!(),
+            ChainType::ArrangementBackfill => todo!(),
         };
         Ok(executor)
     }
